56402c9ae3719db3606d87760f3695c0a9d87464
林晓 耿 <hyper_glx@hotmail.com>
2022-05-11 07:46:37 +0800

    Initial version

---
+++ DocImgZipper/DocImgZipper/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace DocImgZipper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(597, 25);
+            this.textBox1.TabIndex = 0;
+            this.toolTip1.SetToolTip(this.textBox1, "输入文件路径，或者点击右边browser按钮浏览文件。");
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(615, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(517, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 74);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Process!";
+            this.toolTip1.SetToolTip(this.button2, "处理图片。原文件会被备份为.bak文件。");
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 43);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(499, 74);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(0, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(279, 25);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "1.0";
+            this.toolTip1.SetToolTip(this.textBox2, "允许文档中图像被缩小的比例。数值越小，允许的图像分辨率越高，文件越大。");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Limit the zoom level to:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 123);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(678, 79);
+            this.textBox3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 207);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ DocImgZipper/DocImgZipper/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.Drawing.Imaging;
+using System.Collections.Generic;
+
+namespace DocImgZipper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Word Documents(*.docx; *.doc)| *.docx; *.doc";
+            openFileDialog.Multiselect = false;
+            if( openFileDialog.ShowDialog()==DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog.FileName;
+                textBox1.ReadOnly = true;
+                button2.Enabled = true;
+            }
+        }
+
+        /// <summary>
+        /// Resize the image to the specified width and height.
+        /// </summary>
+        /// <param name="image">The image to resize.</param>
+        /// <param name="width">The width to resize to.</param>
+        /// <param name="height">The height to resize to.</param>
+        /// <returns>The resized image.</returns>
+        public Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+
+        public Stream ToStream(Image image, ImageFormat format)
+        {
+            var stream = new System.IO.MemoryStream();
+            image.Save(stream, format);
+            stream.Position = 0;
+            return stream;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            try
+            {
+                System.IO.File.Copy(textBox1.Text, textBox1.Text + ".bak");
+
+                using (WordprocessingDocument document = WordprocessingDocument.Open(textBox1.Text, true))
+                {
+
+                    string tempDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+                    Directory.CreateDirectory(tempDir);
+
+                    Document doc = document.MainDocumentPart.Document;
+
+                    Dictionary<string, float> dImgZoom = new Dictionary<string, float>();
+
+                    int iBlip = 0;
+
+                    foreach (DocumentFormat.OpenXml.Drawing.Pictures.Picture pic in doc.Descendants<DocumentFormat.OpenXml.Drawing.Pictures.Picture>())
+                    {
+                        iBlip++;
+                        string s = pic.OuterXml;
+                        var blip = pic.BlipFill.Blip.Embed.Value;
+                        ImagePart imagePart = (ImagePart)doc.MainDocumentPart.GetPartById(blip);
+                        Image oriImage = Bitmap.FromStream(imagePart.GetStream());
+                        long oriH = oriImage.Height;
+                        long oriW = oriImage.Width;
+                        float oriVRes = oriImage.VerticalResolution;
+                        float oriHRes = oriImage.HorizontalResolution;
+
+                        float spH = pic.ShapeProperties.Transform2D.Extents.Cy.Value * oriVRes / 914400;
+                        float spW = pic.ShapeProperties.Transform2D.Extents.Cx.Value * oriHRes / 914400;
+
+                        float hZoom = spH / oriH;
+                        float vZoom = spW / oriW;
+
+                        if (dImgZoom.ContainsKey(blip))
+                        {
+                            if (dImgZoom[blip] < Math.Max(hZoom, vZoom))
+                                dImgZoom[blip] = Math.Max(hZoom, vZoom);
+                        }
+                        else
+                            dImgZoom[blip] = Math.Max(hZoom, vZoom);
+
+                        //RadioButton radioButton = this.Controls.OfType<RadioButton>()
+                        //    .Where(x => x.Checked).FirstOrDefault();
+
+
+
+                        //if(radioButton!=null)
+                        //{
+                        //    switch(radioButton.Name)
+                        //    {
+                        //        case "radioButton1":
+
+                        //            break;
+                        //        case "radioButton2":
+                        //            break;
+                        //        case "radioButton3":
+                        //            break;
+                        //        case "radioButton4":
+                        //            break;
+                        //        case "radioButton5":
+                        //            break;
+
+                        //    }
+                        //}
+
+                        //Image newImage = ResizeImage(oriImage, 500, 500);
+
+                        //imagePart.FeedData(ToStream(newImage, ImageFormat.Jpeg));
+
+                        ////ImagePart newImgPart = doc.MainDocumentPart.AddImagePart(oriImage.RawFormat.ToString());
+
+
+                        //string uriPath = imagePart.Uri.ToString().TrimStart('/');
+                        //uriPath = uriPath.Replace('/', '\\');
+                        //string tempFilename = Path.Combine(tempDir, uriPath);
+                        //Directory.CreateDirectory(Path.GetDirectoryName(tempFilename));
+                        //oriImage.Save(tempFilename);
+                        //pic.BlipFill.Blip.Embed = doc.MainDocumentPart.GetIdOfPart(imagePart);
+
+
+                    }
+
+                    textBox3.AppendText(iBlip.ToString() + " images found in the file, referencing " + dImgZoom.Count + " actual images.");
+                    textBox3.AppendText(Environment.NewLine);
+
+                    int iImg = 0;
+   
+                    foreach (string blip in dImgZoom.Keys)
+                    {
+                        ImagePart imagePart = (ImagePart)doc.MainDocumentPart.GetPartById(blip);
+                        Image oriImage = Bitmap.FromStream(imagePart.GetStream());
+
+                        float tgtZoomLevel = float.Parse(textBox2.Text);
+
+                        if (dImgZoom[blip] < tgtZoomLevel)
+                        {
+                            iImg++;
+                            Image newImage = ResizeImage(oriImage, (int)(oriImage.Width * dImgZoom[blip] / tgtZoomLevel), (int)(oriImage.Height * dImgZoom[blip] / tgtZoomLevel));
+                            imagePart.FeedData(ToStream(newImage, ImageFormat.Jpeg));
+                        }
+                    }
+                    textBox3.AppendText(iImg.ToString() + " actual image(s) down scaled.");
+                    textBox3.AppendText(Environment.NewLine);
+                }
+
+
+            }
+            catch (Exception exp)
+            {
+                textBox3.AppendText(exp.Message);
+                textBox3.AppendText(Environment.NewLine);
+            }
+            textBox3.AppendText("Process done!");
+            textBox3.AppendText(Environment.NewLine);
+            button2.Enabled = true;
+        }
+    }
+}
