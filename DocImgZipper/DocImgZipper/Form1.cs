ef86807bfeac7ef1f80962250c8d171fa3cd9873
林晓 耿 <hyper_glx@hotmail.com>
2022-05-11 09:37:43 +0800

    Try to support PPT/PPTX file format.

--- DocImgZipper/DocImgZipper/Form1.cs
+++ DocImgZipper/DocImgZipper/Form1.cs
@@ -13,8 +13,8 @@ using System.IO;
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
+using DocumentFormat.OpenXml.Presentation;
 using System.Drawing.Imaging;
-using System.Collections.Generic;
 
 namespace DocImgZipper
 {
@@ -28,7 +28,7 @@ namespace DocImgZipper
         private void button1_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Word Documents(*.docx; *.doc)| *.docx; *.doc";
+            openFileDialog.Filter = "Word Documents(*.docx; *.doc)| *.docx; *.doc|PowerPoint Presentaton(*.pptx;*.ppt)|*.pptx;*.ppt";
             openFileDialog.Multiselect = false;
             if( openFileDialog.ShowDialog()==DialogResult.OK)
             {
@@ -85,107 +85,216 @@ namespace DocImgZipper
             try
             {
                 System.IO.File.Copy(textBox1.Text, textBox1.Text + ".bak");
-
-                using (WordprocessingDocument document = WordprocessingDocument.Open(textBox1.Text, true))
+                string filename = textBox1.Text;
+                if (filename.EndsWith(".doc") || filename.EndsWith(".docx"))
                 {
-
-                    string tempDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
-                    Directory.CreateDirectory(tempDir);
-
-                    Document doc = document.MainDocumentPart.Document;
-
-                    Dictionary<string, float> dImgZoom = new Dictionary<string, float>();
-
-                    int iBlip = 0;
-
-                    foreach (DocumentFormat.OpenXml.Drawing.Pictures.Picture pic in doc.Descendants<DocumentFormat.OpenXml.Drawing.Pictures.Picture>())
+                    using (WordprocessingDocument document = WordprocessingDocument.Open(filename, true))
                     {
-                        iBlip++;
-                        string s = pic.OuterXml;
-                        var blip = pic.BlipFill.Blip.Embed.Value;
-                        ImagePart imagePart = (ImagePart)doc.MainDocumentPart.GetPartById(blip);
-                        Image oriImage = Bitmap.FromStream(imagePart.GetStream());
-                        long oriH = oriImage.Height;
-                        long oriW = oriImage.Width;
-                        float oriVRes = oriImage.VerticalResolution;
-                        float oriHRes = oriImage.HorizontalResolution;
 
-                        float spH = pic.ShapeProperties.Transform2D.Extents.Cy.Value * oriVRes / 914400;
-                        float spW = pic.ShapeProperties.Transform2D.Extents.Cx.Value * oriHRes / 914400;
+                        string tempDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+                        Directory.CreateDirectory(tempDir);
 
-                        float hZoom = spH / oriH;
-                        float vZoom = spW / oriW;
+                        Document doc = document.MainDocumentPart.Document;
 
-                        if (dImgZoom.ContainsKey(blip))
+                        Dictionary<string, float> dImgZoom = new Dictionary<string, float>();
+
+                        int iBlip = 0;
+
+                        foreach (DocumentFormat.OpenXml.Drawing.Pictures.Picture pic in doc.Descendants<DocumentFormat.OpenXml.Drawing.Pictures.Picture>())
                         {
-                            if (dImgZoom[blip] < Math.Max(hZoom, vZoom))
+                            iBlip++;
+                            string s = pic.OuterXml;
+                            var blip = pic.BlipFill.Blip.Embed.Value;
+                            ImagePart imagePart = (ImagePart)doc.MainDocumentPart.GetPartById(blip);
+                            Image oriImage = Bitmap.FromStream(imagePart.GetStream());
+                            long oriH = oriImage.Height;
+                            long oriW = oriImage.Width;
+                            float oriVRes = oriImage.VerticalResolution;
+                            float oriHRes = oriImage.HorizontalResolution;
+
+                            float spH = pic.ShapeProperties.Transform2D.Extents.Cy.Value * oriVRes / 914400;
+                            float spW = pic.ShapeProperties.Transform2D.Extents.Cx.Value * oriHRes / 914400;
+
+                            float hZoom = spH / oriH;
+                            float vZoom = spW / oriW;
+
+                            if (dImgZoom.ContainsKey(blip))
+                            {
+                                if (dImgZoom[blip] < Math.Max(hZoom, vZoom))
+                                    dImgZoom[blip] = Math.Max(hZoom, vZoom);
+                            }
+                            else
                                 dImgZoom[blip] = Math.Max(hZoom, vZoom);
+
+                            //RadioButton radioButton = this.Controls.OfType<RadioButton>()
+                            //    .Where(x => x.Checked).FirstOrDefault();
+
+
+
+                            //if(radioButton!=null)
+                            //{
+                            //    switch(radioButton.Name)
+                            //    {
+                            //        case "radioButton1":
+
+                            //            break;
+                            //        case "radioButton2":
+                            //            break;
+                            //        case "radioButton3":
+                            //            break;
+                            //        case "radioButton4":
+                            //            break;
+                            //        case "radioButton5":
+                            //            break;
+
+                            //    }
+                            //}
+
+                            //Image newImage = ResizeImage(oriImage, 500, 500);
+
+                            //imagePart.FeedData(ToStream(newImage, ImageFormat.Jpeg));
+
+                            ////ImagePart newImgPart = doc.MainDocumentPart.AddImagePart(oriImage.RawFormat.ToString());
+
+
+                            //string uriPath = imagePart.Uri.ToString().TrimStart('/');
+                            //uriPath = uriPath.Replace('/', '\\');
+                            //string tempFilename = Path.Combine(tempDir, uriPath);
+                            //Directory.CreateDirectory(Path.GetDirectoryName(tempFilename));
+                            //oriImage.Save(tempFilename);
+                            //pic.BlipFill.Blip.Embed = doc.MainDocumentPart.GetIdOfPart(imagePart);
+
+
                         }
-                        else
-                            dImgZoom[blip] = Math.Max(hZoom, vZoom);
 
-                        //RadioButton radioButton = this.Controls.OfType<RadioButton>()
-                        //    .Where(x => x.Checked).FirstOrDefault();
+                        textBox3.AppendText(iBlip.ToString() + " images found in the file, referencing " + dImgZoom.Count + " actual images.");
+                        textBox3.AppendText(Environment.NewLine);
 
+                        int iImg = 0;
 
-
-                        //if(radioButton!=null)
-                        //{
-                        //    switch(radioButton.Name)
-                        //    {
-                        //        case "radioButton1":
-
-                        //            break;
-                        //        case "radioButton2":
-                        //            break;
-                        //        case "radioButton3":
-                        //            break;
-                        //        case "radioButton4":
-                        //            break;
-                        //        case "radioButton5":
-                        //            break;
-
-                        //    }
-                        //}
-
-                        //Image newImage = ResizeImage(oriImage, 500, 500);
-
-                        //imagePart.FeedData(ToStream(newImage, ImageFormat.Jpeg));
-
-                        ////ImagePart newImgPart = doc.MainDocumentPart.AddImagePart(oriImage.RawFormat.ToString());
-
-
-                        //string uriPath = imagePart.Uri.ToString().TrimStart('/');
-                        //uriPath = uriPath.Replace('/', '\\');
-                        //string tempFilename = Path.Combine(tempDir, uriPath);
-                        //Directory.CreateDirectory(Path.GetDirectoryName(tempFilename));
-                        //oriImage.Save(tempFilename);
-                        //pic.BlipFill.Blip.Embed = doc.MainDocumentPart.GetIdOfPart(imagePart);
-
-
-                    }
-
-                    textBox3.AppendText(iBlip.ToString() + " images found in the file, referencing " + dImgZoom.Count + " actual images.");
-                    textBox3.AppendText(Environment.NewLine);
-
-                    int iImg = 0;
-   
-                    foreach (string blip in dImgZoom.Keys)
-                    {
-                        ImagePart imagePart = (ImagePart)doc.MainDocumentPart.GetPartById(blip);
-                        Image oriImage = Bitmap.FromStream(imagePart.GetStream());
-
-                        float tgtZoomLevel = float.Parse(textBox2.Text);
-
-                        if (dImgZoom[blip] < tgtZoomLevel)
+                        foreach (string blip in dImgZoom.Keys)
                         {
-                            iImg++;
-                            Image newImage = ResizeImage(oriImage, (int)(oriImage.Width * dImgZoom[blip] / tgtZoomLevel), (int)(oriImage.Height * dImgZoom[blip] / tgtZoomLevel));
-                            imagePart.FeedData(ToStream(newImage, ImageFormat.Jpeg));
+                            ImagePart imagePart = (ImagePart)doc.MainDocumentPart.GetPartById(blip);
+                            Image oriImage = Bitmap.FromStream(imagePart.GetStream());
+
+                            float tgtZoomLevel = float.Parse(textBox2.Text);
+
+                            if (dImgZoom[blip] < tgtZoomLevel)
+                            {
+                                iImg++;
+                                Image newImage = ResizeImage(oriImage, (int)(oriImage.Width * dImgZoom[blip] / tgtZoomLevel), (int)(oriImage.Height * dImgZoom[blip] / tgtZoomLevel));
+                                imagePart.FeedData(ToStream(newImage, ImageFormat.Jpeg));
+                            }
                         }
+                        textBox3.AppendText(iImg.ToString() + " actual image(s) down-scaled.");
+                        textBox3.AppendText(Environment.NewLine);
                     }
-                    textBox3.AppendText(iImg.ToString() + " actual image(s) down scaled.");
-                    textBox3.AppendText(Environment.NewLine);
+
+                }
+                if (filename.EndsWith(".ppt")|| filename.EndsWith(".pptx"))
+                {
+                    using (PresentationDocument document = PresentationDocument.Open(filename, true))
+                    {
+
+                        string tempDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+                        Directory.CreateDirectory(tempDir);
+
+                        PresentationPart doc = document.PresentationPart;
+
+                        Dictionary<string, float> dImgZoom = new Dictionary<string, float>();
+
+                        int iBlip = 0;
+
+                        foreach (DocumentFormat.OpenXml.Drawing.Pictures.Picture pic in doc.Presentation.Descendants<DocumentFormat.OpenXml.Drawing.Pictures.Picture>())
+                        {
+                            iBlip++;
+                            string s = pic.OuterXml;
+                            var blip = pic.BlipFill.Blip.Embed.Value;
+                            ImagePart imagePart = (ImagePart)doc.MainDocumentPart.GetPartById(blip);
+                            Image oriImage = Bitmap.FromStream(imagePart.GetStream());
+                            long oriH = oriImage.Height;
+                            long oriW = oriImage.Width;
+                            float oriVRes = oriImage.VerticalResolution;
+                            float oriHRes = oriImage.HorizontalResolution;
+
+                            float spH = pic.ShapeProperties.Transform2D.Extents.Cy.Value * oriVRes / 914400;
+                            float spW = pic.ShapeProperties.Transform2D.Extents.Cx.Value * oriHRes / 914400;
+
+                            float hZoom = spH / oriH;
+                            float vZoom = spW / oriW;
+
+                            if (dImgZoom.ContainsKey(blip))
+                            {
+                                if (dImgZoom[blip] < Math.Max(hZoom, vZoom))
+                                    dImgZoom[blip] = Math.Max(hZoom, vZoom);
+                            }
+                            else
+                                dImgZoom[blip] = Math.Max(hZoom, vZoom);
+
+                            //RadioButton radioButton = this.Controls.OfType<RadioButton>()
+                            //    .Where(x => x.Checked).FirstOrDefault();
+
+
+
+                            //if(radioButton!=null)
+                            //{
+                            //    switch(radioButton.Name)
+                            //    {
+                            //        case "radioButton1":
+
+                            //            break;
+                            //        case "radioButton2":
+                            //            break;
+                            //        case "radioButton3":
+                            //            break;
+                            //        case "radioButton4":
+                            //            break;
+                            //        case "radioButton5":
+                            //            break;
+
+                            //    }
+                            //}
+
+                            //Image newImage = ResizeImage(oriImage, 500, 500);
+
+                            //imagePart.FeedData(ToStream(newImage, ImageFormat.Jpeg));
+
+                            ////ImagePart newImgPart = doc.MainDocumentPart.AddImagePart(oriImage.RawFormat.ToString());
+
+
+                            //string uriPath = imagePart.Uri.ToString().TrimStart('/');
+                            //uriPath = uriPath.Replace('/', '\\');
+                            //string tempFilename = Path.Combine(tempDir, uriPath);
+                            //Directory.CreateDirectory(Path.GetDirectoryName(tempFilename));
+                            //oriImage.Save(tempFilename);
+                            //pic.BlipFill.Blip.Embed = doc.MainDocumentPart.GetIdOfPart(imagePart);
+
+
+                        }
+
+                        textBox3.AppendText(iBlip.ToString() + " images found in the file, referencing " + dImgZoom.Count + " actual images.");
+                        textBox3.AppendText(Environment.NewLine);
+
+                        int iImg = 0;
+
+                        foreach (string blip in dImgZoom.Keys)
+                        {
+                            ImagePart imagePart = (ImagePart)doc.MainDocumentPart.GetPartById(blip);
+                            Image oriImage = Bitmap.FromStream(imagePart.GetStream());
+
+                            float tgtZoomLevel = float.Parse(textBox2.Text);
+
+                            if (dImgZoom[blip] < tgtZoomLevel)
+                            {
+                                iImg++;
+                                Image newImage = ResizeImage(oriImage, (int)(oriImage.Width * dImgZoom[blip] / tgtZoomLevel), (int)(oriImage.Height * dImgZoom[blip] / tgtZoomLevel));
+                                imagePart.FeedData(ToStream(newImage, ImageFormat.Jpeg));
+                            }
+                        }
+                        textBox3.AppendText(iImg.ToString() + " actual image(s) down-scaled.");
+                        textBox3.AppendText(Environment.NewLine);
+                    }
+
                 }
 
 
